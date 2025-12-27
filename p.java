3258200ac10749b59a051c8d7a5f4a532ad76538
Nikhil <kujurnikhil0007@gmail.com>
2025-12-27 12:03:45 -0800

    Prime number code

---
+++ p.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class p{
+    public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter number");
+int num = sc.nextInt();
+
+  
+    
+        if(num < 1){
+      System.out.println("Invalid number ");
+        return ;
+    }
+
+if(num == 1){
+     System.out.println("Its a prime number");
+}
+
+boolean isPrime = true;
+  for(int i = 2; i*i<num ; i++){
+     if( num % i == 0){
+        isPrime = false;
+        break;
+     }
+  }    
+
+  if(isPrime){
+    System.out.println("Its a prime number" + " "+ num);
+  }else{
+     System.out.println("Its not a prime number"+ " "+ num);
+  }
+
+  
+}
+}
